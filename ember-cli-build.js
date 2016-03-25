2ef411b0b028180f1af203ffb0a4b1652d7f8a0e
Joel Sabillon <js1894@yp.com>
2016-03-25 16:53:35 -0700

    renamed blueprints to correct name, added included hook to index.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,8 +13,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  
- app.import('bower_components/ag-grid/dist/ag-grid.js');
+
 
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-ag-grid'
+  name: 'ember-ag-grid',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/ag-grid/dist/ag-grid.js');
+  }
 };
