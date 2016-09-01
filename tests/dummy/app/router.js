ed990e09014bea7d9e67f7b80733d2cec95a8661
Joel Sabillon <js1894@yp.com>
2016-09-01 10:45:38 -0700

    updating to ember cli 2.7.0 and emberjs 2.7.1

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
@@ -15,4 +16,3 @@ Router.map(function() {
 });
 
 export default Router;
-  
